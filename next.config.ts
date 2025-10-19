5700d6831cd8ecd52ea801455dc385378ecbb90c
Bala <132751639+souravkumar-cloud@users.noreply.github.com>
2025-10-19 14:50:34 +0530

    logo

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: true,
-    serverActionsBodySizeLimit: 10 * 1024 * 1024, // 10 MB
-  },
   images: {
     remotePatterns: [
       {
